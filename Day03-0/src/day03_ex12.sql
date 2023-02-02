47210cfd86cea48dac426961d3f62e4fd2d2dbd3
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-02-02 20:58:44 +0300

    ex12.v2

--- Day03-0/src/day03_ex12.sql
+++ Day03-0/src/day03_ex12.sql
@@ -1,5 +1,5 @@
-INSERT INTO person_order VALUES (generate_series((SELECT max(id) + 1 AS fuck FROM person_order)
-    , (SELECT (max(id) + (SELECT(count(DISTINCT person_id)) FROM person_order)) AS fuck FROM person_order), '1'),
-    generate_series((SELECT min(person_id) AS fuck FROM person_order)
-    , (SELECT max(person_id) AS fuck FROM person_order),
+INSERT INTO person_order VALUES (generate_series((SELECT max(id) + 1 FROM person_order)
+    , (SELECT (max(id) + (SELECT(count(DISTINCT person_id)) FROM person_order)) FROM person_order), '1'),
+    generate_series((SELECT min(person_id) FROM person_order)
+    , (SELECT max(person_id) FROM person_order),
     '1'), (SELECT id FROM menu WHERE pizza_name = 'greek pizza'), '2022-02-25');
