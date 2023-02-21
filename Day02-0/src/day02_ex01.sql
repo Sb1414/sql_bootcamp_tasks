d920bad3f0bb3d07435fe8bc27dc943f98ab8cf2
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-02-21 21:04:28 +0300

    modified:   Day02-0/src/day02_ex01.sql

--- Day02-0/src/day02_ex01.sql
+++ Day02-0/src/day02_ex01.sql
@@ -1,3 +1,10 @@
 SELECT DISTINCT visit_date AS missing_date FROM person_visits
 WHERE visit_date BETWEEN '2022-01-01' AND '2022-01-10' AND visit_date NOT IN (SELECT visit_date FROM person_visits
 WHERE (person_id = 1 OR person_id = 2));
+
+-- SELECT CAST(missing_date AS DATE)
+-- FROM generate_series('2022-01-01', '2022-01-10', interval '1 day') missing_date
+-- FULL JOIN (SELECT visit_date FROM person_visits
+-- WHERE (person_id = 1 OR person_id = 2) AND visit_date BETWEEN '2022-01-01' AND '2022-01-10') AS t
+-- ON missing_date = t.visit_date WHERE t.visit_date IS  NULL
+-- ORDER BY missing_date;
