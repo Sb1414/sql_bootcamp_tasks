33b04b76abc84bb3905703404949fe633aaeb931
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-02-21 21:04:48 +0300

    modified:   Day02-0/src/day02_ex04.sql

--- Day02-0/src/day02_ex04.sql
+++ Day02-0/src/day02_ex04.sql
@@ -4,7 +4,7 @@ TABLE_MENU_CTE (pizzeria_id, pizza_name, price)
 AS (SELECT pizzeria_id, pizza_name, price FROM menu
 WHERE pizza_name = 'mushroom pizza' OR pizza_name = 'pepperoni pizza')
 
-SELECT pizza_name AS Pizza_name, name AS pizzeria_name, price
+SELECT pizza_name, name AS pizzeria_name, price
 FROM TABLE_PIZZA_CTE
 JOIN TABLE_MENU_CTE on pizzeria_id = id
 ORDER BY pizza_name, pizzeria_name;
