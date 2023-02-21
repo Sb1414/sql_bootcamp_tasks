303b919bbf2ec505c994b3a8fef6fc6da49887ef
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-02-21 21:04:14 +0300

    modified:   Day02-0/src/day02_ex00.sql

--- Day02-0/src/day02_ex00.sql
+++ Day02-0/src/day02_ex00.sql
@@ -1,3 +1,7 @@
+-- SELECT name, rating FROM pizzeria
+-- WHERE name NOT IN (SELECT name FROM pizzeria
+-- JOIN person_visits pv on pizzeria.id = pv.pizzeria_id);
+
 SELECT name, rating FROM pizzeria
-WHERE name NOT IN (SELECT name FROM pizzeria
-JOIN person_visits pv on pizzeria.id = pv.pizzeria_id);
+LEFT JOIN person_visits  on pizzeria.id = pizzeria_id
+WHERE pizzeria_id is NULL;
